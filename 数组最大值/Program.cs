db100d4325ebf7ae5883af293e36e20a94b3a0c1
niko <66620705+nikoLua@users.noreply.github.com>
2022-11-12 02:21:15 +0800

    Add files via upload

---
+++ 数组最大值/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 数组最大值
+{
+    class Program
+    {
+        static int MaxElement(int[] array)
+        {
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            int[] g = new int[] { 2, 5, 6, 4, 7 };
+            
+            Console.WriteLine(MaxElement(g));
+
+            Console.ReadKey();
+        }
+    }
+}
